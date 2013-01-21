6ccdcc80c8dd58fef4f1773ce9945b054a30e61f
Tomasz Dziuda <dziudek@gmail.com>
2013-01-21 16:54:40 +0100

    Removed unnecessary hgroup tag

--- MeetGavernWP/layouts/content.post.header.php
+++ MeetGavernWP/layouts/content.post.header.php
@@ -20,23 +20,21 @@ global $tpl;
 <?php endif; ?>
 
 <?php if(get_the_title() != '') : ?>
-<hgroup>
-	<h<?php echo (is_single()) ? '1' : '2'; ?>>
-		<?php if(!is_singular()) : ?>
-		<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', GKTPLNAME ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark">
-		<?php endif; ?>
-			<?php the_title(); ?>
-		<?php if(!is_singular()) : ?>
-		</a>
-		<?php endif; ?>
-		
-		<?php if(is_sticky()) : ?>
-		<sup>
-			<?php _e( 'Featured', GKTPLNAME ); ?>
-		</sup>
-		<?php endif; ?>
-	</h<?php echo (is_single()) ? '1' : '2'; ?>>
-</hgroup>
+<h<?php echo (is_single()) ? '1' : '2'; ?>>
+	<?php if(!is_singular()) : ?>
+	<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', GKTPLNAME ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark">
+	<?php endif; ?>
+		<?php the_title(); ?>
+	<?php if(!is_singular()) : ?>
+	</a>
+	<?php endif; ?>
+	
+	<?php if(is_sticky()) : ?>
+	<sup>
+		<?php _e( 'Featured', GKTPLNAME ); ?>
+	</sup>
+	<?php endif; ?>
+</h<?php echo (is_single()) ? '1' : '2'; ?>>
 <?php endif; ?>
 
 <?php do_action('gavernwp_before_post_content'); ?>
