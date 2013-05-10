813e8d41be791424bc4fd2c32fb9bd10a78f7306
dziudek <dziudek@gmail.com>
2013-05-10 15:39:03 +0200

    Improved the author name in the gk_author_block
    Earlier if user didn't specified the first and last name the username
    wasn't displayed.

--- MeetGavernWP/gavern/helpers/helpers.layout.fragments.php
+++ MeetGavernWP/gavern/helpers/helpers.layout.fragments.php
@@ -521,7 +521,7 @@ function gk_author($author_page = false, $return_value = false) {
 			        <div class="author-desc">
 			            <h2>
 			                <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-			                    <?php printf( __( 'Author: %s %s', GKTPLNAME ), get_the_author_meta('first_name', get_the_author_meta( 'ID' )), get_the_author_meta('last_name', get_the_author_meta( 'ID' )) ); ?> 
+			                    <?php printf( __( 'Author: %s ', GKTPLNAME ), get_the_author_meta('display_name', get_the_author_meta( 'ID' )) ); ?> 
 			                </a>
 			            </h2>
 			            <p>
