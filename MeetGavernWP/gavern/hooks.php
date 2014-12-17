acfb1acc16ea446d3926984d338a47734fbac862
piotrkunicki <piotrkunicki@gavick.com>
2014-12-17 13:06:40 +0100

    Added support for the Universal google analitics code.

--- MeetGavernWP/gavern/hooks.php
+++ MeetGavernWP/gavern/hooks.php
@@ -223,18 +223,33 @@ function gavernwp_ga_code_hook() {
 	// check if the Tracking ID is specified
 	if(get_option($tpl->name . '_ga_ua_id', '') != '') {
 		?>
-		<script type="text/javascript">
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', '<?php echo get_option($tpl->name . '_ga_ua_id', ''); ?>']);
-		  _gaq.push(['_trackPageview']);
-		
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-		</script>
-		<?php
+
+		<?php if(get_option($tpl->name . '_ga_ua_type', 'ga') == 'ga' ) : ?>
+			<script type="text/javascript">
+			  var _gaq = _gaq || [];
+			  _gaq.push(['_setAccount', '<?php echo get_option($tpl->name . '_ga_ua_id', ''); ?>']);
+			  _gaq.push(['_trackPageview']);
+			
+			  (function() {
+			    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			  })();
+			</script>
+
+		<?php else : ?>
+			<script>
+			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			  ga('create', '<?php echo get_option($tpl->name . '_ga_ua_id', ''); ?>', 'auto');
+			  ga('send', 'pageview');
+
+			</script>
+		<?php endif; 
+
 	}
 }
   
